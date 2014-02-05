8534e66d49a337290e874ced4e00f22de00a6765
Kevin Lustig <klustig@Kevins-MacBook-Pro-2.local>
2014-02-05 14:14:27 -0600

    began working on tests for codebreaker_submits_guess feature

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -5,17 +5,17 @@ module Codebreaker
     describe "#start" do
       before(:each) do
         @output = double('output').as_null_object
-        @game = Game.new(output)
+        @game = Game.new(@output)
       end
       
       it "sends a welcome message" do
         @output.should_receive(:puts).with('Welcome to Codebreaker!')
-        @game.start
+        @game.start('1234')
       end
 
       it "prompts for the first guess" do
         @output.should_receive(:puts).with('Enter guess:')
-        @game.start
+        @game.start('1234')
       end
 
     end
